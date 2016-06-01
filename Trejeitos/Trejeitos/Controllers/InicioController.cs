8c1a4d8af21b158159533a8964c41d035d9f6e00
eesteves <eric_evanescence@yahoo.com.br>
2016-05-31 23:07:33 -0300

    Pagina inicial agora busca os produtos no banco de dados e os exibe na tela, ordenação por maior e menor preço implementadas com sucesso.

--- Trejeitos/Trejeitos/Controllers/InicioController.cs
+++ Trejeitos/Trejeitos/Controllers/InicioController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Trejeitos.Models;
 
 namespace Trejeitos.Controllers
 {
@@ -11,9 +12,29 @@ namespace Trejeitos.Controllers
         // GET: Inicio
         public ActionResult Inicio()
         {
-            return View();
+                ListProdutoModel list = new ListProdutoModel();
+                List<ListProduto> lista = list.ListarVitrine();
+
+                return View(lista);
         }
 
+        public ActionResult OrdenaPrecoDesc()
+        {
+            ListProdutoModel list = new ListProdutoModel();
+            List<ListProduto> lista = new List<ListProduto>();
+            lista = list.ListarVitrineDesc();
+
+            return View("Inicio", lista);
+        }
+
+        public ActionResult OrdenaPrecoAsc()
+        {
+            ListProdutoModel list = new ListProdutoModel();
+            List<ListProduto> lista = new List<ListProduto>();
+            lista = list.ListarVitrineAsc();
+
+            return View("Inicio", lista);
+        }
         public ActionResult Carrinho()
         {
             return RedirectToAction("Carrinho", "Carrinho");
