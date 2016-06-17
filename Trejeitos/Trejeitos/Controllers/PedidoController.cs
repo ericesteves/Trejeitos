9121cee67570f9a65fd934aced6cc9e1f1a41bb9
eesteves <eric_evanescence@yahoo.com.br>
2016-06-17 15:47:32 -0300

    Tela de pedido funcionando parcialmente

--- Trejeitos/Trejeitos/Controllers/PedidoController.cs
+++ Trejeitos/Trejeitos/Controllers/PedidoController.cs
@@ -13,5 +13,20 @@ namespace Trejeitos.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult CalculaParcela(FormCollection form)
+        {
+            decimal total = (decimal)(Session["total"]);
+            decimal parcela = 0;
+            parcela = total / decimal.Parse(form["parcela"]);
+            ViewBag.parcela = parcela;
+            TempData["cartao"] = form["cartao"];
+            TempData["nome"] = form["nome"];
+            TempData["mes"] = form["mes"];
+            TempData["ano"] = form["ano"];
+
+
+            return View("Pedido");
+        }
     }
 }
